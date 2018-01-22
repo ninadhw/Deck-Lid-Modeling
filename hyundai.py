3a40e2fb4e7f9f1373c91d3b27f6dab2212d79aa
Ninad H W <nind.watve@gmail.com>
2018-01-23 00:04:50 +0530

    changes in file that dont lead to anything

--- hyundai.py
+++ hyundai.py
@@ -1,6 +1,6 @@
 #hyundai code for deck lid
 import numpy as np
-import scipy.integrate as sp
+from scipy.integrate import ode
 import matplotlib.pyplot as mpl
 
 r1=80           #mm
@@ -41,29 +41,50 @@ def calculate_angle_41 (a1,a2,a3,a4,theta21):
     atan=np.rad2deg(atan)
     return [atan[1]]
 
-ni=180 - (phi_0 + sci_0)
+##ni=180 - (phi_0 + sci_0)
+##theta41 = calculate_angle_41(r1,r2,r3,r4,(180-20-40))
+##theta31 = calculate_angle_41(r2,r1,r4,r3,(180-20-40))
+##
+##print(theta31)
+##print(theta41)
+th1=[]
+th2=[]
+def solvern(t , U):
 
+    Mw= W* ((rg*np.cos(np.deg2rad(theta_g - U[0]))) + (r2*np.cos(np.deg2rad(180-sci_0-U[0]))))
 
-# Theta 41 = calculate_angle_41(r1,r2,r3,r4,ni)
-# theta41=np.matrix.transpose(np.rad2deg(ninad))
-# Theta 31 = calculate_angle_41(r2,r1,r4,r3,ni)
-# theta31=np.matrix.transpose(np.rad2deg(ninad))
-
-def solvern(U, t):
-    Mw= W* ( (rg*np.cos(np.deg2rad(theta_g - U[0]))) + (r2*np.cos(np.deg2rad(180 - sci_0 - U[0]))) )
     theta41 = calculate_angle_41 (r1,r2,r3,r4,U[0])
     theta31 = calculate_angle_41 (r2,r1,r4,r3,U[0])
-    theta31 = theta31[0] - U[0]
+    theta31 = -theta31[0] + U[0]
+    th1.append(theta31)
+    th2.append(theta41)
+    #print('theta41:',theta41[0])
+
     torsionbar= sci_0 + beta_0 + theta41[0]
     Mb=bk*(np.deg2rad(torsionbar))
-    Mt=2*Mb*(r2*np.sin(np.deg2rad(U[0] - theta31)) ) /  (r4*np.sin(np.deg2rad(theta41[0] - theta31)))
-    return [U[1], (     (-k1*U[1]) + (-k2*U[1]**2) + (Mw-Mt)     )/Ik]
+    Mt=2*Mb*(r2*np.sin(np.deg2rad(U[0] - theta31))) /(r4*np.sin(np.deg2rad(theta41[0] - theta31)))
+    
+    return [U[1], -1*(((-k1*U[1]) + (-k2*U[1]**2) + (-Mw+Mt))/Ik)]
 
-xs = np.linspace(0,50,500)
-Us = sp.odeint(solvern, [120, 0], xs)
 
-U1 = Us[:,1]
+r=ode(solvern, jac=None).set_integrator('dopri5', nsteps=1000)
+r.set_initial_value([120,0],0)
+t1=100
+dt=0.1
 
-mpl.plot(xs,U1)
+
+i=0
+disp=[]
+vel=[]
+th1=[]
+th2=[]
+while r.successful() and r.t<t1:
+    b=r.integrate(r.t+dt)
+    disp.append(b[0])
+    vel.append(b[1])
+    i=i+1
+    
+tzone=np.linspace(0,t1,(t1/dt))
+mpl.plot(disp,'r')
+mpl.plot(vel,'b--')
 mpl.show()
-print(Us)
